ff4afd0f54cd4adaab5024b6e34485b9d8da46bd
joaovitor3 <joaovytor0@gmail.com>
2021-10-24 14:08:43 -0300

    Add run experiment script

--- bot/run_experiment.py
+++ bot/run_experiment.py
@@ -1,13 +1,59 @@
 import mlflow
-import os
+import yaml
+from test_cli import run_tests
+import os.path
 
 
-mlflow.set_experiment('exp')
+def file_exists(filename):
+    return os.path.isfile(filename) 
 
-# Verify problems with permissions (possible because of missing boto3 dependency)
-# https://stackoverflow.com/questions/52331254/how-to-store-artifacts-on-a-server-running-mlflow
+def yaml_to_dict(filename: str):
+    with open(filename, "r") as yaml_file:
+        try:
+            yaml_dict = yaml.safe_load(yaml_file)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return yaml_dict
 
-with mlflow.start_run():
-    # The experiment MUST have artifact URI configured
-    mlflow.log_artifact('models/20210827-034140.tar.gz')
-    print(f'Artifact URI {mlflow.get_artifact_uri()}')
+def log_artifacts(model_filepath: str, config_filepath: str, domain_filepath: str):
+    mlflow.log_artifact(model_filepath)
+    mlflow.log_artifact(config_filepath)
+    mlflow.log_artifact(domain_filepath)
+
+def log_metrics(failed_stories_filepath: str):
+    failed_stories = 0
+    if file_exists(failed_stories_filepath):
+        failed_stories_dict = yaml_to_dict(failed_stories_filepath)
+        failed_stories = len(failed_stories_dict["stories"])
+
+    mlflow.log_metric("failed_stories", failed_stories)
+
+
+if __name__ == "__main__":
+    domain_dict = yaml_to_dict("domain.yml")
+    entities = domain_dict["entities"]
+    intents = domain_dict["intents"]
+
+    config_dict = yaml_to_dict("config.yml")
+    pipeline = config_dict["pipeline"]
+    policies = config_dict["policies"]
+
+    model_filepath = 'models/20210827-034140.tar.gz'
+    model_params = {
+        "pipeline": pipeline,
+        "policies": policies
+    }
+
+    config_filepath = 'config.yml'
+    domain_filepath = "domain.yml"
+    nlu_filepath = "data/nlu.yml"
+    rules_filepath = "data/rules.yml"
+    stories_filepath = "data/stories.yml"
+
+    # run_tests()
+    mlflow.set_experiment('exp')
+    with mlflow.start_run():
+        log_artifacts(model_filepath, config_filepath, domain_filepath)
+        log_metrics("results/failed_test_stories.yml")
+
+    
--- bot/test_cli.py
+++ bot/test_cli.py
@@ -32,27 +32,28 @@ logger.setLevel('DEBUG')
 #    url=None
 #)
 
-args = Namespace(
-        config=None,
-        cross_validation=False,
-        stories='.',
-        endpoints='endpoints.yml',
-        out='results/',
-        no_errors=False,
-        no_warnings=False,
-        model='models',
-        nlu='data',
-        evaluate_model_directory=False,
-        fail_on_prediction_errors=False,
-        percentages=[0,
-        25,
-        50,
-        75],
-        max_stories=None,
-        runs=3,
-        e2e=True,
-        successes=False,
-        warnings=True
-)
-test.run_core_test(args)
-test.run_nlu_test(args)
+def run_tests():
+	args = Namespace(
+	        config=None,
+	        cross_validation=False,
+	        stories='.',
+	        endpoints='endpoints.yml',
+	        out='results/',
+	        no_errors=False,
+	        no_warnings=False,
+	        model='models',
+	        nlu='data',
+	        evaluate_model_directory=False,
+	        fail_on_prediction_errors=False,
+	        percentages=[0,
+	        25,
+	        50,
+	        75],
+	        max_stories=None,
+	        runs=3,
+	        e2e=True,
+	        successes=False,
+	        warnings=True
+	)
+	test.run_core_test(args)
+	test.run_nlu_test(args)
